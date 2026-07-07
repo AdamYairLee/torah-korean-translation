bba48a9f54fc97317dfb63dad7b03c3ea36e823f
AdamYairLee <koreanjewish@gmail.com>
2026-07-07 22:37:48 +0300

    올리브영 제휴 연결

--- AI-Korean/js/config.js
+++ AI-Korean/js/config.js
@@ -3,6 +3,6 @@ const CONFIG = {
   SITE_NAME: "AI Korean Master",
   VERSION: "1.4.1 International Beta",
   WHATSAPP_URL: "https://wa.me/972502188989?text=Hello!%20I%20am%20interested%20in%20your%20Korean%20learning%20platform.%20I%20would%20like%20to%20discuss%20a%20business%20partnership.",
-  OLIVE_YOUNG_URL: "#",
+  OLIVE_YOUNG_URL: "https://global.oliveyoung.com/if/rd?su=15E8PLIK",
   TRAVEL_URL: "#"
 };
